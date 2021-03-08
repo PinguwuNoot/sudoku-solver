886e03d20f432b154b4ddd4b677655e2ee40bac2
PinguwuNoot <hanjasn@gmail.com>
2021-03-07 21:53:58 -0800

    updated ui

--- src/ui.py
+++ src/ui.py
@@ -2,7 +2,26 @@ from model import SudokuBoard
 
 
 class CUI:
-    pass
+    BOARD_1 = [
+        [7, 0, 0, 0, 0, 9, 0, 0, 3],
+        [0, 9, 0, 1, 0, 0, 8, 0, 0],
+        [0, 1, 0, 0, 0, 7, 0, 0, 0],
+        [0, 3, 0, 4, 0, 0, 0, 8, 0],
+        [6, 0, 0, 0, 8, 0, 0, 0, 1],
+        [0, 7, 0, 0, 0, 2, 0, 3, 0],
+        [0, 0, 0, 5, 0, 0, 0, 1, 0],
+        [0, 0, 4, 0, 0, 3, 0, 9, 0],
+        [5, 0, 0, 7, 0, 0, 0, 0, 2]
+    ]
+
+    def __init__(self):
+        self.solver = SudokuBoard(CUI.BOARD_1)
+        self.run()
+
+    def run(self):
+        print("Puzzle:\n{}".format(self.solver))
+        self.solver.solve()
+        print("Solution:\n{}".format(self.solver))
 
 
 class GUI:
